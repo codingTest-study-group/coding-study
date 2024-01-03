a0bf421a6b0fe8659a35de5866fe9e7eee5faa01
Dongyeop Lee <dlduq29@gmail.com>
2024-01-04 00:54:25 +0900

    프로그래머스 lv2 최솟값 만들기

---
+++ lee/src/programmers/ex_MinNum.java
@@ -0,0 +1,23 @@
+package programmers;
+import java.util.Arrays;
+public class ex_MinNum {
+    public static void main(String[] args) {
+        int[] A = {1,4,2};
+        int[] B = {5,4,4};
+
+        System.out.println(solution(A,B));
+    }
+    public static int solution(int[] A, int[] B)
+    {
+        int answer =0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i = 0; i<A.length; i++){
+            answer += A[i] * B[B.length-i-1];
+        }
+
+        return answer;
+    }
+}
